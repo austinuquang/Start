f51d588011f716d51d54f5747f73123fa8cbcc1a
Austin Uquang <austinuquang@gmail.com>
2024-10-03 17:01:13 +0100

    first commit

---
+++ Overview/MyConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string response;
+Console.WriteLine("What is your name?");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+response = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+Console.WriteLine($"Enjoy the course, {response}");
+
+OperatingSystem thisOS = Environment.OSVersion;
+Console.WriteLine(thisOS.Platform);
+Console.WriteLine(thisOS.VersionString);
